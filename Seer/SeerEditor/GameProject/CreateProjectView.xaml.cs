8f5487c87e93e1ab3391bf09fd8f632642796b4e
TimHyers <hyerst@gmail.com>
2022-07-29 18:03:15 -0400

    Added Basic functionality to create a project

---
+++ Seer/SeerEditor/GameProject/CreateProjectView.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SeerEditor.GameProject
+{
+    /// <summary>
+    /// Interaction logic for CreateProjectView.xaml
+    /// </summary>
+    public partial class CreateProjectView : UserControl
+    {
+        public CreateProjectView()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void OnCreate_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var vm = DataContext as CreateProject;
+            var projectPath = vm.NewProject(templateListBox.SelectedItem as ProjectTemplate);
+            bool dialogResult = false;
+            var win = Window.GetWindow(this);
+            if(!string.IsNullOrEmpty(projectPath))
+            {
+                dialogResult = true;
+            }
+            win.DialogResult = dialogResult;
+            win.Close();
+        }
+    }
+}
---
+++ Seer/SeerEditor/GameProject/OpenProject.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeerEditor.GameProject
+{
+    class OpenProject :ViewModelBase
+    {
+    }
+}
