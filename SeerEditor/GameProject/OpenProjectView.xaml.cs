13e3e75a1c8f20f50a96ba54e91bea6ff4c6dbb0
TimHyers <hyerst@gmail.com>
2023-02-28 23:19:10 -0500

    Added ability to add scenes to the worldview. Added basic undo/redo

--- SeerEditor/GameProject/OpenProjectView.xaml.cs
+++ SeerEditor/GameProject/OpenProjectView.xaml.cs
@@ -23,6 +23,13 @@ namespace SeerEditor.GameProject
         public OpenProjectView()
         {
             InitializeComponent();
+            Loaded += (s, e) =>
+            {
+                var item =
+                    projectsListBox.ItemContainerGenerator.ContainerFromIndex(projectsListBox.SelectedIndex) as
+                        ListBoxItem;
+                item?.Focus();
+            };
         }
 
         private void OnOpen_Button_Click(object sender, RoutedEventArgs e)
--- SeerEditor/GameProject/Project.cs
+++ SeerEditor/GameProject/Project.cs
@@ -5,10 +5,12 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net.Mime;
+using System.Runtime.InteropServices;
 using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 using SeerEditor.Utilities;
 
 namespace SeerEditor.GameProject
@@ -46,6 +48,25 @@ namespace SeerEditor.GameProject
 
         public static Project CurrentProject => Application.Current.MainWindow.DataContext as Project;
 
+        public static UndoRedo UndoRedo { get; } = new UndoRedo();
+
+        public ICommand Undo { get; private set; }
+        public ICommand Redo { get; private set; }
+
+        public ICommand AddScene { get; private set; }
+        public ICommand RemoveScene { get; private set; }
+
+        private void AddSceneInternal(string sceneName)
+        {
+            Debug.Assert(!string.IsNullOrEmpty(sceneName.Trim()));
+            _scenes.Add((new Scene(this, sceneName)));
+        }
+        private void RemoveSceneInternal(Scene scene)
+        {
+            Debug.Assert(_scenes.Contains(scene));
+            _scenes.Remove(scene);
+        }
+
         public static Project Load(string file)
         {
             Debug.Assert(File.Exists(file));
@@ -70,6 +91,33 @@ namespace SeerEditor.GameProject
             }
 
             ActiveScene = Scenes.FirstOrDefault(x => x.IsActive);
+
+            AddScene = new RelayCommand<object>(x =>
+            {
+                AddSceneInternal($"New Scene {_scenes.Count}");
+                var newScene = _scenes.Last();
+                var sceneIndex = _scenes.Count -1;
+
+                UndoRedo.Add(new UndoRedoAction(
+                    () => RemoveSceneInternal(newScene),
+                    () => _scenes.Insert(sceneIndex, newScene),
+                    $"Add {newScene.Name}"));
+            });
+
+            RemoveScene = new RelayCommand<Scene>(x =>
+            {
+                var sceneIndex = _scenes.IndexOf(x);
+                RemoveSceneInternal(x);
+
+                UndoRedo.Add(new UndoRedoAction(
+                    () => _scenes.Insert(sceneIndex, x),
+                    () => RemoveSceneInternal(x),
+                    $"Remove {x.Name}"
+                ));
+            }, x => !x.IsActive);
+
+            Undo = new RelayCommand<object>(x => UndoRedo.Undo());
+            Redo = new RelayCommand<object>(x => UndoRedo.Redo());
         }
 
         public Project(string name, string path)
